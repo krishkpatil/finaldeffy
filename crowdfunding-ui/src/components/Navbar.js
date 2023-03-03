4581aff58dafcc6a635d0c744fcebd2c1ebe684e
krishkpatil <patilkkrish@gmail.com>
2023-03-03 10:09:06 +0530

    commit nav

--- crowdfunding-ui/src/components/Navbar.js
+++ crowdfunding-ui/src/components/Navbar.js
@@ -25,53 +25,70 @@ function Example() {
 
     return (
         <div className="bg-transparent fixed top-0 w-full z-10 -mt-4">
-            <div className="max-w-screen-xl mx-auto px-6 pt-4 pb-8">
+            <div className=" mx-auto px-6 pt-4 pb-8">
                 <div className="bg-[#09162A] shadow-lg rounded-lg p-4 h-20 flex items-center justify-center">
                     <Navbar
                         color="bg-[#0a192f]"
                         dark
                         expand="md"
-                        className="flex flex-wrap items-center justify-between w-full"
+                        fixed="top"
+                        className="flex flex-wrap items-center justify-between p-4 w-full mx-auto"
                     >
                         <div className="flex items-center">
                             <Link
                                 to="/"
-                                className="text-white font-bold text-3xl mr-4 ml-4 no-underline"
+                                className="text-white font-bold text-3xl mr-2 ml-2 no-underline"
                             >
                                 Deffyfunds
                             </Link>
                         </div>
-                        <Nav navbar className="justify-center">
-                            <NavItem>
-                                <NavLink
-                                    tag={Link}
-                                    to="/create"
-                                    className="text-gray-200 hover:text-pink-600 mr-4"
-                                >
-                                    Create a Campaign
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink
-                                    tag={Link}
-                                    to="/campaigns"
-                                    className="text-gray-200 hover:text-pink-600 mr-4"
-                                >
-                                    Campaigns
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink
-                                    tag={Link}
-                                    to="/about"
-                                    className="text-gray-200 hover:text-pink-600 mr-4"
-                                >
-                                    About
-                                </NavLink>
-                            </NavItem>
-                        </Nav>
-                        <div>
-                            <WalletMultiButton />
+                        <button
+                            className="navbar-toggler"
+                            type="button"
+                            onClick={toggle}
+                        >
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div
+                            className={`${
+                                isOpen ? 'block' : 'hidden'
+                            } w-full flex-grow md:flex md:items-center md:w-auto`}
+                        >
+                            <Nav
+                                navbar
+                                className="text-md md:flex-grow md:justify-center"
+                            >
+                                <NavItem>
+                                    <NavLink
+                                        tag={Link}
+                                        to="/create"
+                                        className="text-gray-200 hover:text-pink-600 block md:inline-block mr-4"
+                                    >
+                                        Create a Campaign
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink
+                                        tag={Link}
+                                        to="/campaigns"
+                                        className="text-gray-200 hover:text-pink-600 block md:inline-block mr-4"
+                                    >
+                                        Campaigns
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink
+                                        tag={Link}
+                                        to="/about"
+                                        className="text-gray-200 hover:text-pink-600 block md:inline-block mr-4"
+                                    >
+                                        About
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                            <div>
+                                <WalletMultiButton />
+                            </div>
                         </div>
                     </Navbar>
                 </div>
